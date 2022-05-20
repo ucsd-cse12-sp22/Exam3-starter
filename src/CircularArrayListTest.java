80ce14e7f162be0990924d625477303345d17196
Sang555 <38208028+Sang555@users.noreply.github.com>
2022-05-20 10:46:49 -0700

    Update CircularArrayListTest.java

--- src/CircularArrayListTest.java
+++ src/CircularArrayListTest.java
@@ -6,20 +6,9 @@ public class CircularArrayListTest {
 	
 	@Test
 	public void test_baseCase() throws Exception {
-		// You don't have to use array list 
-		// This test will work with any object that implements List
+
 		ArrayListADT<Integer> cal = new CircularArrayList<Integer> (10);
-		cal.addFront(12);
-		cal.addFront(8);
-		cal.addRear(1);
-		cal.addRear(2);
-		cal.addFront(7);
-		cal.remove();
-		//5 10 1 4
-		assertEquals(Integer.valueOf(12), cal.get(1));
-		assertEquals(Integer.valueOf(8), cal.get(0));
-		assertEquals(Integer.valueOf(1), cal.get(2));
-		assertEquals(Integer.valueOf(2), cal.get(3));
+		//Complete testcase to check elements at few positions
 	}
 	
 }
