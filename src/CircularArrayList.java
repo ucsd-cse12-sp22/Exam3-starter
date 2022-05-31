8b49aae0e88f451ae4987266dd11efeb30c4efa1
Sang555 <38208028+Sang555@users.noreply.github.com>
2022-05-30 20:20:07 -0700

    Update CircularArrayList.java

--- src/CircularArrayList.java
+++ src/CircularArrayList.java
@@ -1,10 +1,11 @@
+import java.util.Iterator;
 
-public class CircularArrayList<T> implements ArrayListADT<T>{
-	
+public class CircularArrayList<T> implements ArrayListADT<T>,Iterable<T>{
 	int capacity;
 	int size;
 	int front;
 	int rear;
+	int it;
 	T[] arrayList;
 	
 	public CircularArrayList() {
@@ -18,6 +19,7 @@ public class CircularArrayList<T> implements ArrayListADT<T>{
 
 	@Override
 	public void addRear(T element) {
+		
 		//Method to add element at the rear of the arraylist
 	}
 
@@ -25,21 +27,45 @@ public class CircularArrayList<T> implements ArrayListADT<T>{
 	public void addFront(T element) {
 		
 		//Method to add element at the front of the arraylist i.e. towards start
-		
 	}
 
 	public T get(int index) throws Exception {
-		
 		//Method to get element at a given index of the circularArrayList
 	}
 	
-
-
+	
 	@Override
 	public T remove() {
-		
-		//Method to remove the element from the front
+		//Method to remove element from the front
 	}
 	
+	 public class MyArrayListIterator<T> implements Iterator<T> {
+		    private int currentIndex = front; //start with front index
+
+		    @Override
+		    public boolean hasNext() {
+		      //Method to check if iterator has next element
+		    }
+
+		    @Override
+		    public T next() {
+		    	//Used to get the next element of iterator
+		    }
+		  }
+
+	@Override
+	public Iterator<T> iterator() {
+		return  new MyArrayListIterator();
+	}
+
+	@Override
+	public int getSize() {
+		//Fill method to get size of arraylist
+	}
+
+	@Override
+	public int getCapacity() {
+		//Fill method to get capacity of arraylist
+	}
 	
 }
