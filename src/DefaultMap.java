00f5694684f3e891b1d08e384261e73b257191f0
Sang555 <38208028+Sang555@users.noreply.github.com>
2022-05-30 20:39:01 -0700

    Update DefaultMap.java

--- src/DefaultMap.java
+++ src/DefaultMap.java
@@ -1,27 +1,48 @@
-import java.util.Comparator;
-import java.util.List;
-
 public interface DefaultMap<K, V> {
 	public static final Comparator myComparator = null;
-	
+	/**
+	 * Internal Representation of every (key, value) pair in this DefaultMap
+	 */
 	interface Entry<K, V> {
 		K getKey();
 		V getValue();
-		
+
 		void setValue(V value);
 	}
 
-	
+	/**
+	 * Adds the specified key, value pair to this DefaultMap
+	 * 
+	 * @return true if the key value pair was added to this DefaultMap
+	 * @throws IllegalArgument exception if the key is null
+	 */
 	boolean put(K key, V value) throws IllegalArgumentException;
-	// To put the key-value pair in the map
-	
+
+
+	/**
+	 * @return the value corresponding to the specified key, null if key doesn't 
+	 * exist in hash map
+	 * @throws IllegalArgument exception if the key is null
+	 */
 	V get(K key) throws IllegalArgumentException;
-	//To get the value(min value of bucket in this example) in the bucket
-	
+
+	/**
+	 * 
+	 * @return The number of (key, value) pairs in this DefaultMap
+	 */
 	int size();
 
+	/**
+	 * 
+	 * @return true iff this.size() == 0 is true
+	 */
 	boolean isEmpty();
 
+
+	/**
+	 * @return true if the specified key is in this DefaultMap
+	 * @throws IllegalArgument exception if the key is null
+	 */
 	boolean containsKey(K key) throws IllegalArgumentException;
 
 }
